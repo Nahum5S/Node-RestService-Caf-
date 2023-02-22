360cfd0f5255944c4abb7f92736d467baec7ada7
Nahum Torres <erick.natg@gmail.com>
2023-02-21 23:06:28 -0600

    Se agrego login por inicio de sesión en google v 3.0.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,10 @@
 const { response } = require("express");
 const bcrypt = require('bcryptjs')
+
 const User = require('../models/user');
+
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async(req, res = response) =>{
 
@@ -49,8 +52,51 @@ const login = async(req, res = response) =>{
 }
 
 
+const googleSignIn = async(req, res = response) =>{
+    const { id_token } = req.body;
 
+    try {
+
+        const {nombre, img, correo} = await googleVerify(id_token);
+
+        let usuario = await User.findOne({correo});
+
+        if( !usuario ){
+            //Tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: 'uwu',
+                img,
+                google: true
+            };
+
+            usuario = new User( data );
+            await usuario.save();
+        }
+
+        // Si el usuario existe en la DB
+        if(!usuario.estado){
+            return res.status(401).json({
+                msg:'Hable con el administrador - Usuario bloqueado'
+            });
+        }
+
+        //Generar el JWT
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario,
+            token
+        })
+    } catch (error) {
+        res.status(400).json({
+            msg: 'El token no se pudo verificar'
+        })
+    }
+}
 
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
